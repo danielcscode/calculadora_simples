4d78eb1d7b284b8bee98ea14638e0e5acf2a3423
Daniel Silva <danielcscode@gmail.com>
2024-03-11 21:37:11 -0300

    Alteração de exibição de valores double e int!

--- src/Principal.java
+++ src/Principal.java
@@ -4,72 +4,72 @@ public class Principal {
  
     public static void main(String[] args) {
         Scanner dados = new Scanner(System.in); 
-        double porcentagem = 0, resultadoFinal = 0, resultadoParcial = 0, resultado = 0, continuar, calculo1, calculo2;
+        int continuar1, continuar2, calculo3 = 0, calculo4 = 0, resultadoInteiro = 0, resultadoParcial = 0, resultadoFinal = 0;
+        double porcentagem = 0, resultadoPorcentagem = 0, calculo1, calculo2;
         String operador;
+
         System.out.println();
         System.out.println("Bem vindo a calculadora Back end!!!");
         System.out.println("A operação vai usar porcentagem? [1]- Sim / [2] Não?");
-        continuar = dados.nextInt();
-        if (continuar == 1) {
+        continuar1 = dados.nextInt();
+
+        if (continuar1 == 1) {
             System.out.println("Digite o primeiro numero:");        
             calculo1 = dados.nextDouble();
             System.out.println("Digite o segundo numero em porcentagem:");
             calculo2 = dados.nextDouble();
-
-
             porcentagem = calculo2 / 100;
-            resultado = calculo1 * porcentagem;
-            System.out.println("Resultado Final: "+ resultado);
+            resultadoPorcentagem = calculo1 * porcentagem;
+            System.out.println("Resultado Final: "+ resultadoPorcentagem);
             System.out.println("ENCERRADO!");
         } else {
             System.out.println("Digite o primeiro numero:");
-            calculo1 = dados.nextDouble();
+            calculo3 = dados.nextInt();
             System.out.println("Digite o operador:");
             operador = dados.next();
             System.out.println("Digite o segundo numero:");
-            calculo2 = dados.nextDouble(); 
+            calculo4 = dados.nextInt(); 
             
             if(operador.equals("+")) {
-                resultado = calculo1 + calculo2;
+                resultadoFinal = calculo3 + calculo4;
             }else if(operador.equals("-")){
-                resultado = calculo1 - calculo2;
+                resultadoFinal = calculo3 - calculo4;
             }else if(operador.equals("*")){
-                resultado = calculo1 * calculo2; 
+                resultadoFinal = calculo3 * calculo4; 
             }else if(operador.equals("/")){
-                resultado = calculo1 / calculo2;
+                resultadoFinal = calculo3 / calculo4;
             }    
         
-            System.out.println("Resultado Final: "+ resultado);
+            System.out.println("Resultado Final: "+ resultadoFinal);
             System.out.println("Deseja continuar [1]- Sim / [2] Não?");
-            continuar = dados.nextInt();
+            continuar2 = dados.nextInt();
                 
-            if (continuar == 1) {
+            if (continuar2 == 1) {
                 do {
-                    resultadoParcial = resultado;
+                    resultadoParcial = resultadoFinal;
         
                     System.out.println("Digite o operador:");
-
                     operador = dados.next();
                     System.out.println("Digite o segundo numero:");
-                    calculo2 = dados.nextDouble(); 
+                    calculo4 = dados.nextInt(); 
                         
                     if(operador.equals("+")) {
-                            resultado = resultadoParcial + calculo2;
-                        }else if(operador.equals("-")){
-                            resultado = resultadoParcial - calculo2;
-                        }else if(operador.equals("*")){
-                            resultado = resultadoParcial * calculo2; 
-                        }else if(operador.equals("/")){
-                            resultado = resultadoParcial / calculo2;
-                        }
-                        System.out.println("Resultado Final: "+ resultado);
+                        resultadoFinal = resultadoParcial + calculo4;
+                    }else if(operador.equals("-")){
+                        resultadoFinal = resultadoParcial - calculo4;
+                    }else if(operador.equals("*")){
+                        resultadoFinal = resultadoParcial * calculo4; 
+                    }else if(operador.equals("/")){
+                        resultadoFinal = resultadoParcial / calculo4;
+                    }
+                        System.out.println("Resultado Final: "+ resultadoFinal);
                         System.out.println("Deseja continuar [1]- Sim / [2] Não?");
-                        continuar = dados.nextInt();    
-                } while(continuar == 1);
-                System.out.println("Resultado Final: "+ resultado);
+                        continuar2 = dados.nextInt();    
+                } while(continuar2 == 1);
+                System.out.println("Resultado Final: "+ resultadoFinal);
                 System.out.println("ENCERRADO!");
                 } else {
-                    System.out.println("Resultado Final: "+ resultado);
+                    System.out.println("Resultado Final: "+ resultadoFinal);
                     System.out.println("ENCERRADO!");
                 }
             }
